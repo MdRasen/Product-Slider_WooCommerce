3e693eff177402e101674353eb02ab5acba15af0
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-02-21 14:22:36 +0600

    Swiper JS version updated

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-// selecting dom element
-const element = document.querySelector(".carousel"); //add your selector name .class/#id
-
-// call the function slider parameters:-
-// 1. parent/main element
-// 2. Object for options -eg: {auto:true}
-const mySlider = slider(element, { auto: false });
-
-const previousBtn = document.querySelector("#pre");
-const nextBtn = document.querySelector("#nxt");
-
-mySlider.click(previousBtn); //2nd parameter -1 refers to negative positioning & default is +1
-mySlider.click(nextBtn); // default is +1 so you don't have to write twice
-
-const $ = document.querySelector.bind(document);
-const a = slider($(".carousel"), {
-  show: 4, //Number of items shown per page
-  to: 1, //Number of items change
-  responsive: {
-    900: {
-      show: 4,
-      to: 1,
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  // For autoplay feature
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
-    700: {
-      show: 3,
-      to: 1,
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
-    500: {
-      show: 2,
-      to: 1,
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
-    200: {
-      show: 1,
-      to: 1,
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
     },
   },
-  auto: { delay: 2500 },
 });
-
-a.click($("#pre"), -1);
-a.click($("#nxt"));
